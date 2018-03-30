ee34f6350244434840d02bb142ee9f7609f0ffb6
Scott Clegg <scott.c@cox.net>
2018-03-30 12:44:20 -0700

    Update DBW node to 50Hz. Modify contoller to add lpf on velocity, change filter coefficients, add v=0 baking

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -14,12 +14,14 @@ class Controller(object):
                  wheel_base, wheel_radius, steer_ratio, max_lat_accel, max_steer_angle):
 
         self.deceleration_limit = deceleration_limit
-        self.velocity_controller = PID(1.5, 0.01, 0., deceleration_limit, acceleration_limit)
+        #self.velocity_controller = PID(1.5, 0.01, 0., deceleration_limit, acceleration_limit)
+        self.velocity_controller = PID(.3, .1, 0., 0., .2)
         self.total_mass = vehicle_mass + (fuel_capacity * GAS_DENSITY)
         self.wheel_radius = wheel_radius
         self.yaw_controller = YawController(wheel_base, steer_ratio, min_speed, max_lat_accel, max_steer_angle)
-        self.steer_lpf = LowPassFilter(3, 1)
-        self.throttle_lpf = LowPassFilter(3, 1)
+        #self.steer_lpf = LowPassFilter(3, 1)
+        #self.throttle_lpf = LowPassFilter(3, 1)
+        self.vel_lpf = LowPassFilter(0.5, 0.02)
         self.brake_deadband = brake_deadband
         self.min_speed = min_speed
 
@@ -27,10 +29,13 @@ class Controller(object):
 	#  Calculating the velocity correction (acceleration/deceleration) by PID
 	#  Return the acceleration (throttle), brake (deceleration) and steer angle 
     def control(self, twist_cmd, current_velocity, time_elapsed):
-        vel_error = twist_cmd.twist.linear.x - current_velocity.twist.linear.x
+        
+        # add lowpass filter to current velocity to remove signal noise
+        current_vel = self.vel_lpf.filt(current_velocity.twist_cmd.linear.x)
+        vel_error = twist_cmd.twist.linear.x - current_vel
         throttle = self.velocity_controller.step(vel_error, time_elapsed)
-        steer = self.yaw_controller.get_steering(twist_cmd.twist.linear.x, twist_cmd.twist.angular.z, current_velocity.twist.linear.x)
-
+        steer = self.yaw_controller.get_steering(twist_cmd.twist.linear.x, twist_cmd.twist.angular.z, current_vel)
+"""
         if current_velocity.twist.linear.x < 0.1 and np.isclose(twist_cmd.twist.linear.x, 0.):
             torque = self.total_mass * self.wheel_radius * self.deceleration_limit
             return 0., torque, steer
@@ -42,6 +47,17 @@ class Controller(object):
                 throttle = 0
             torque = self.total_mass * throttle
             return 0., torque, steer
+"""
+        brake = 0
+        if twist_cmd.twist.linear.x == 0 and current_vel < 0.:
+            throttle = 0
+            brake = 400
+        elif throttle < 0.1 and vel_error < 0:
+            throttle = 0
+            decel = max(vel_error, self.deceleration_limit)
+            brake = abs(decel)*self.vehicle_mass*self.wheel_radius
+        
+        return throttle, brake, steer
 
     def reset(self):
         self.velocity_controller.reset()
